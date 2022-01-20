7c2eda5380e41ae503879536f786dbdb44961dde
Marcia <marcia.garciam@hotmail.com>
2022-01-19 19:33:45 -0600

    fields added to table

--- demo/models.py
+++ demo/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=36)
+    title = models.CharField(blank=False, unique=True, max_length=36)
+    description = models.TextField(blank=True, max_length=256)
+    price = models.DecimalField(default=0, max_digits=3, decimal_places=2)
+    published = models.DateField(blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='covers/', blank=True)
