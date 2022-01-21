7c36646affc181c08951614ff774dad58f5da9a0
Marcia <marcia.garciam@hotmail.com>
2022-01-20 19:35:50 -0600

    first view added

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from .models import Book
 
 # Create your views here.
+class Another(View):
+    # books = Book.objects.all()
+    # books = Book.objects.filter(is_published=True)
+    books = Book.objects.get(id=2) # gets one record only
+    # output = ''
+    # for book in books:
+    #     output += f"we have {book.title} with ID {book.id} in db<br>"
+
+    output = f"we have {books.title} with ID {books.id} in db<br>"
+
+    def get(self, request):
+        return HttpResponse(self.output)
+
+def first(request):
+    return HttpResponse('first message')
