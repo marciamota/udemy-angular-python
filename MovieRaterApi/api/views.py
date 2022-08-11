c281f4636d32919ab17fc297c7a34dc073186823
Marcia <marcia.garciam@hotmail.com>
2022-08-10 19:43:30 -0500

    20220810

--- MovieRaterApi/api/views.py
+++ MovieRaterApi/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -15,10 +16,24 @@ class MovieViewSet(viewsets.ModelViewSet):
         if 'stars' in request.data:
 
             movie = Movie.objects.get(id=pk)
-            print('movie title', movie.title)
-            
-            response = {'message': 'its working'}
-            return Response(response, status=status.HTTP_200_OK)
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
+            print('user', user.username)
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
         else:
             response = {'message': 'You need to provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
